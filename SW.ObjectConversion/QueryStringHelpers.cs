0d20d632f6bec56e5c18e569b711cac738c80c0f
Shaheen Sarafa <shaheen@simplify9.com>
2020-08-04 11:02:35 +0300

    added query string generation from object

--- SW.ObjectConversion/QueryStringHelpers.cs
+++ SW.ObjectConversion/QueryStringHelpers.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Primitives;
 using SW.PrimitiveTypes;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Cache;
@@ -11,7 +12,53 @@ namespace SW.ObjectConversion
 {
     public static class QueryStringHelpers
     {
-        public static object GetFromQueryString(Type type, HttpRequest httpRequest)
+        public static string GetQueryStringFromObject<T>(T obj) where T : class
+        {
+            string[] props = typeof(T).GetProperties()
+                            .Select(property => {
+
+                                bool isArray = property.PropertyType.IsArray;
+                                bool isCollection = property.PropertyType.GetInterfaces()
+                                                    .Where(i => i.IsGenericType)
+                                                    .Select(i => i.GetGenericTypeDefinition())
+                                                    .Contains(typeof(ICollection<>));
+                                if (isArray || isCollection)
+                                {
+                                    IEnumerable tmp = ((IEnumerable)property.GetValue(obj).ConvertValueToType(property.PropertyType));
+                                    IEnumerable<string> enumerable = tmp.Cast<string>();
+
+                                    Type nested = property.PropertyType.GetElementType() ?? property.PropertyType.GetGenericArguments()[0];
+
+                                    int length = 0;
+                                    foreach (var val in tmp) ++length;
+
+                                    Array array = Array.CreateInstance(nested, length);
+
+                                    int counter = 0;
+                                    foreach (var val in tmp)
+                                    {
+                                        array.SetValue(val, counter);
+                                        ++counter;
+                                    }
+
+                                    string q = string.Empty;
+                                    for (int i = 0; i < array.Length - 1; i++) q += $"{property.Name}={array.GetValue(i)}&";
+                                    q += $"{property.Name}={array.GetValue(array.Length - 1)}";
+
+                                    return q;
+                                }
+                                else return $"{property.Name}={property.GetValue(obj)}";
+                            })
+                            .ToArray();
+
+            string queryString = string.Empty;
+            for (int i = 0; i < props.Count() - 1; i++) queryString += props[i] + '&';
+            queryString += props[props.Length - 1];
+
+            return queryString;
+        }
+
+        public static object GetObjectFromQueryString(Type type, HttpRequest httpRequest)
         {
             try
             {
@@ -23,7 +70,6 @@ namespace SW.ObjectConversion
                     StringValues queries = httpRequest.Query[property.Name];
 
                     bool isArray = property.PropertyType.IsArray;
-
                     bool isCollection = property.PropertyType.GetInterfaces()
                                         .Where(i => i.IsGenericType)
                                         .Select(i => i.GetGenericTypeDefinition())
